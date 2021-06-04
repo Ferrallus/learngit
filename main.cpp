14e7b7b0b1b6640ccfa439a5a21b0b347ab3c518
Ferrallus <41847415+Ferrallus@users.noreply.github.com>
2021-06-04 21:12:18 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/types/FILE.h>
-#include<iostream>
-#include<stdio.h>
-#include<cmath>
-using namespace std;
-int main(){
-    std::cout<<"Hello World!"<<endl;
-    cout<<"I'M THE BEST"<<endl;
-    }
