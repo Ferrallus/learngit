e0dd78513bb5c9c0f2c8b947563ae671960ce34b
sddx-qn <sddx-qn@sddxqn-MS-7B94>
2021-06-04 20:57:40 +0800

    cs

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <bits/types/FILE.h>
-include<iostream>
+#include<iostream>
 #include<stdio.h>
 #include<cmath>
 using namespace std;
 int main(){
     std::cout<<"Hello World!"<<endl;
     cout<<"I'M THE BEST"<<endl;
-    //intt a
-
-//}
+    }
